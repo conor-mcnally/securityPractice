a8ac2cc5ceb3694709226dddb80a8b0797020a67
conor-mcnally <cmcn@gmx.co.uk>
2019-10-27 00:19:39 +0100

    Add files via upload

--- bruteForce.py
+++ bruteForce.py
@@ -1,6 +1,4 @@
-#key = 4 characters long, lowercase letters only
-
-#try every possible combination of 4 characters as the key
+#python .\bruteForce.py .\ciphertexts.txt
 
 #STEP ONE: ITERATE THROUGH DICTIONARY
 #STEP TWO: FILTER TO 4 LETTER WORDS
@@ -14,18 +12,37 @@ wordLength = int(input("Enter length of word: "))
 
 file = open("words.txt", "r")
 
-def characters4():
-    for line in file:
-        if (len(line) == (wordLength + 1)):
-            words.append(line.lower())
 
-    #Remove the \n that appears at the end of each word
-    for i in range (0, len(words)):
-        words[i] = words[i][:-1]
+for line in file:
+    if (len(line) == (wordLength + 1)):
+        words.append(line.lower())
 
-    print(words)
-    return words
+#Remove the \n that appears at the end of each word
+for i in range (0, len(words)):
+    words[i] = words[i][:-1]
 
-characters4()
+#testing
+print(words)
+
+
+
+input_file = open(sys.argv[1])
+input_str = input_file.read()
+
+
+for i in words:
+    key = i
+
+
+    no_of_itr = (len(input_str))
+    output_str = ""
+
+    for i in range(no_of_itr):
+        current = input_str[i]
+        current_key = key[i%len(key)]
+        output_str += chr(ord(current) ^ ord(current_key))
+
+    print("Heres the output: ", output_str)
+
 # file.close()
 
